5d798efb8c2350def2cad27e1a151bf0a0980b66
Zhong76 <zhonglix76@gmail.com>
2025-08-24 12:47:23 -0500

    Cambios

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MailService } from './mail.service';
-
+import { Public } from 'src/auth/public.decorator';
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
-
+@Public()
   @Post('send')
   async sendMail(
     @Body('to') to: string,
--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -55,7 +55,7 @@ export class RequestsController {
   async getOne(@Param('id') id: string) {
     const data = await this.service.getOne(Number(id));
     return { success: true, data };
-  }
+  } 
 @Public()
   @Post()
   @ApiOperation({ summary: 'Crear solicitud' })
